e6b182eb9ff7404daf5983d5348a69973f120fc0
Nitesh Pandey <112393577+nitesh-ui@users.noreply.github.com>
2022-11-23 16:23:16 +0530

    Add files via upload

---
+++ operators/filter.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+interface NewsItem = {
+    category: 'Business' | 'Sports';
+    content: string
+}
+
+const newsFeed = new Observable<NewsItem>(subscriber => {
+    setTimeout(() => subscriber.next({category: 'Business', content: 'A'}), 1000);
+    setTimeout(() => subscriber.next({category: 'Sports', content: 'B'}), 2000);
+    setTimeout(() => subscriber.next({category: 'Business', content: 'C'}), 3000);
+    setTimeout(() => subscriber.next({category: 'Business', content: 'D'}), 4000);
+    setTimeout(() => subscriber.next({category: 'Business', content: 'E'}), 5000);
+});
+
+newsFeed.
+    .pipe(
+        .filter((item) => item.category === 'Sports')
+    )
+    subscribe((item) => console.log(item))
